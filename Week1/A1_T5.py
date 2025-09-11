3e5765bb14b50799a60963b579b7335a30d6b64b
ToinZaa <hannonentoni@gmail.com>
2025-09-11 15:47:11 +0300

    Weektasks

---
+++ Week1/A1_T5.py
@@ -0,0 +1,6 @@
+print("Calculate the area of a wall.")
+Width = int(input("Enter the width in meters: "))
+Height = int(input("Enter the height in meters: "))
+print("Width is", Width, "m and height is", Height, "m.")
+Area = Width * Height
+print("The wall will be", Area, "square meters.")
